fde00912f8b79aa31813da19fd6fa2819a398295
AnesuMuusha <anesumuusha@gmail.com>
2024-07-03 08:20:20 +0200

    activated nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-function NavBar() {
+function NavBar({ onSelectGenre }) {
+  const genres = [
+    { id: 1, name: 'Personal Growth' },
+    { id: 2, name: 'Investigative Journalism' },
+    { id: 3, name: 'History' },
+    { id: 4, name: 'Comedy' },
+    { id: 5, name: 'Entertainment' },
+    { id: 6, name: 'Business' },
+    { id: 7, name: 'Fiction' },
+    { id: 8, name: 'News' },
+    { id: 9, name: 'Kids and Family' },
+  ];
+
   return (
-    <div>Home | Personal Growth | Investigative Journalism | History | Comedy | Entertainment | Business | Fiction | News | Kids and Family </div>
-
-)
+    <div>
+      <span onClick={() => onSelectGenre(null)} style={{ cursor: 'pointer', marginRight: '10px' }}>Home</span> | 
+      {genres.map((genre) => (
+        <span key={genre.id} onClick={() => onSelectGenre(genre.id)} style={{ cursor: 'pointer', marginLeft: '10px' }}>
+          {genre.name}
+        </span>
+      ))}
+    </div>
+  );
 }
 
 export default NavBar;
