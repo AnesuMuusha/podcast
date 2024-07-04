88112f063d85adb6807e79b99d58a95c8aeb0a31
AnesuMuusha <anesumuusha@gmail.com>
2024-07-04 20:22:33 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import PodcastDetails from './components/PodcastDetails';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/podcast/:id" element={<PodcastDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
